4fb522c545d3043f034e56082f940e3925a9af46
gaze9999 <gaze9999@livemail.tw>
2020-01-30 05:47:22 +0800

    fix cookie

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   let cookies = Cookies.get()
   for (i in cookies) {
-    if (i != 'cid') {
+    if (i.slice(0, 5) == 'link_') {
       let linkEmbed = `https://www.youtube.com/embed/${cookies[i]}?autoplay=1`,
           cookieId = i.slice(5)
       wall.append(
