7d7afc58e00ea3cd52511f2bf9819fb2b979c529
gaze9999 <gaze9999@livemail.tw>
2020-01-30 05:42:18 +0800

    add cookie and clear all auto adjust height

---
+++ src/js/cookie.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  let cookies = Cookies.get()
+  for (i in cookies) {
+    if (i != 'cid') {
+      let linkEmbed = `https://www.youtube.com/embed/${cookies[i]}?autoplay=1`,
+          cookieId = i.slice(5)
+      wall.append(
+        `<div class="wrapper" data-id="${cookieId}">      
+          <button class="linkRemove frame_btn">刪除</button>
+          <button class="linkLoop frame_btn">循環播放</button>
+          <iframe type="text/html" src="${linkEmbed}"></iframe>
+        </div>`
+      )
+    }
+  }
+  cid = parseInt(Cookies.get('cid', { path: '' }))
+  changeHeight()
+})
--- src/js/linkFilter.js
+++ src/js/linkFilter.js
@@ -5,11 +5,14 @@
 let link = $('.linkInput'),
     submitLink = $('.linkSubmit'),
     removeLink = $('.linkRemove'),
+    clearLink = $('.linkClear'),
     wall = $('#wallFrame'),
-    count = 0
+    linkPlayer = $('.wrapper'),
+    cid = 0
 
 submitLink.on('mouseup', function() {
   let thisLink = $.trim(link.val()), getLink
+  !Cookies.get('cid', { path: '' }) && (cid = 0)
 
   thisLink = link.val().split('/')
   thisLink[0] != 'https:' ?
@@ -19,24 +22,49 @@ submitLink.on('mouseup', function() {
   getLink.length > 11 && (
     getLink = getLink.split('watch?v=')[1]
   ) & (
-    getLink = getLink.split('?')[0]
+    getLink = getLink.slice(0, 11)
   )
 
   let linkEmbed = `https://www.youtube.com/embed/${getLink}?autoplay=1`
 
   wall.append(
-    `<div class="wrapper" data-count="${count}">      
-      <button class="linkRemove">刪除</button>
+    `<div class="wrapper" data-id="${cid}">      
+      <button class="linkRemove frame_btn">刪除</button>
+      <button class="linkLoop frame_btn">循環播放</button>
       <iframe type="text/html" src="${linkEmbed}"></iframe>
     </div>`
   )
   link.val('')
-  count += 1
+  Cookies.set(`link_${cid}`, getLink, { expires: 365, path: '' })
+  cid += 1
+  Cookies.set('cid', cid, { expires: 365, path: '' })
+  changeHeight()
 })
 
 wall.on('mouseup', '.linkRemove', function() {
-  // console.log($(this));
-  // let getCount = $(this).parent('div').data('count')
-  // console.log(getCount)
-  getCount = $(this).parent('div').remove()
+  let thisObj = $(this).parent('div'),
+      thiscId = thisObj.data('id')
+  Cookies.remove(`link_${thiscId}`, { path: '' })
+  getCount = thisObj.remove()
+  changeHeight()
 })
+
+clearLink.on('mouseup', function() {  
+  let cookies = Cookies.get()
+  for (i in cookies) {
+    Cookies.remove(i, { path: '' })
+  }  
+  Cookies.remove('cid', { path: '' })
+  linkPlayer = $('.wrapper')
+  linkPlayer.remove()
+})
+
+wall.on('change', function() {
+  changeHeight()
+})
+
+function changeHeight() {
+  let playerFrame = $('.wrapper').find('iframe'),
+      height = playerFrame.width() * 0.5625
+  playerFrame.css('height', height + 'px')
+}
